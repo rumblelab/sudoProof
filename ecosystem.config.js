0b59ec7d719d7f5b422495c8695448e49da20346
moo ball <moo@moos-mbp.lan>
2025-07-10 10:11:38 -0400

    changed gunicorn path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     apps: [{
       name: 'flask-app', // The name of your app in PM2
-      script: 'gunicorn', // The script to run
+      script: 'venv/bin/gunicorn',
       args: '-w 4 -b 0.0.0.0:5000 app:app', // Arguments to pass to Gunicorn
       interpreter: 'venv/bin/python', // The path to the Python interpreter in your virtual environment
       watch: false, // Don't use PM2's watch, we rely on git push
